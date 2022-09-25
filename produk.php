d8421e4756aed523c127694203943b2d29a1d08f
tawakalmit <tawakal.mit@gmail.com>
2022-09-25 23:33:43 +0700

    property and method

--- produk.php
+++ produk.php
@@ -4,8 +4,30 @@
 // komik dan game
 
 class Produk {
+	public $judul, 
+	 	   $penulis,
+	 	   $penerbit,
+	 	   $harga;
+
+	public function getLabel(){
+		return "$this->penulis, $this->penerbit";
+	}
 
 }
 
 $produk1 = new Produk();
+$produk1->judul = "One Piece";
+$produk1->penulis = "Eichiro Oda";
+$produk1->penerbit = "Shonen Jump";
+$produk1->harga = 30000;
+
+echo $produk1->getLabel();
+echo "<br>";
+
 $produk2 = new Produk();
+$produk2->judul = "Naruto";
+$produk2->penulis = "Masashi Kishimoto";
+$produk2->penerbit = "Shonen Jump";
+$produk2->harga = 35000;
+
+echo $produk2->getLabel();
